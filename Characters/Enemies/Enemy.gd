326ba481b9cb2f1ac0dcfc3d766c93f1fc10e1f8
kpsr01 <pranavsuhasreddykonduru@gmail.com>
2025-12-14 18:40:04 +0530

    Replace enemy sprites: Goblin, SlimeBoss, FlyingCreature (Bat)
    - Updated Goblin with new idle, walk, die, hurt animations (150x150 frames)
    
    - Updated SlimeBoss with new slime sprites (32x32 frames)
    
    - Updated FlyingCreature with Bat sprites (48x48 frames)
    
    - Fixed Enemy.gd null reference error (get_node_or_null)
    
    - Organized sprites into Art/Enemies/{Goblin,Slime,Bat}/

--- Characters/Enemies/Enemy.gd
+++ Characters/Enemies/Enemy.gd
@@ -3,7 +3,7 @@
 extends Character
 class_name Enemy
 
-@onready var player: CharacterBody2D = get_tree().current_scene.get_node("Player")
+@onready var player: CharacterBody2D = get_tree().current_scene.get_node_or_null("Player")
 @onready var path_timer: Timer = get_node("PathTimer")
 @onready var navigation_agent: NavigationAgent2D = get_node("NavigationAgent2D")
 
